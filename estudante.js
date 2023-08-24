072f67f76240fdf6a3d41d28b7dc05ec8c54be29
marileoncio <mariana.timoteo19@yahoo.com>
2023-08-24 15:45:14 -0300

    estudante exercicio

---
+++ estudante.js
@@ -0,0 +1,26 @@
+"use strict";
+class Estudante {
+    constructor(nome, idade, notas) {
+        this.nome = nome;
+        this.idade = idade;
+        this.notas = notas;
+    }
+    ;
+    Aluno() {
+        return "Aluno:" + this.nome + " Idade: " + this.idade + " Notas:" + this.notas;
+    }
+    Media() {
+        let soma = 0;
+        for (let i = 0; i < this.notas.length; i++) {
+            soma += this.notas[i];
+        }
+        const media = soma / this.notas.length;
+        if (media < 7) {
+            return this.nome + ", sua média é " + media + "  e você precisa estudar mais para melhorar seu desempenho. ";
+        }
+        return "Parabéns, " + this.nome + " Sua média é " + media + " e você está aprovado(a).";
+    }
+}
+let estudante1 = new Estudante("Brenda", 17, [8, 7.5, 9.2, 6.8, 9.5]);
+console.log(estudante1.Aluno());
+console.log(estudante1.Media());
